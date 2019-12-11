e27a307653bfec80f07b11a800679c3c9649b0b5
Georg Ledermann <mail@georg-ledermann.de>
2019-12-11 19:53:49 +0100

    Add test for exception handler

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -14,6 +14,16 @@ module Dummy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.exceptions_app = ->(env) do
+      Class.new(ActionController::Base) do
+        def show
+          render inertia: 'Error', props: {
+            status: request.path_info[1..-1].to_i
+          }
+        end
+      end.action(:show).call(env)
+    end
   end
 end
 
